748aa4401c2a3ce0d746b44d7505dc1f13d26c63
Lucas Souza <42901468+Showza@users.noreply.github.com>
2021-12-24 00:47:36 -0300

    correções do canvas, timer de resposta e text de vitoria

--- Assets/Scripts/HitCoin.cs
+++ Assets/Scripts/HitCoin.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using System.Threading.Tasks;
 //<summary>
 //Ball movement controlls and simple third-person-style camera
 //</summary>
@@ -13,8 +13,12 @@ public class HitCoin : MonoBehaviour
 
 	private AudioSource mAudioSource = null;
 
+	public bool resposta = false;
+	public TMP_Text win;
+	public TMP_Text lose;
+
 	private GameObject operacao;
-	private GameObject canvas;
+	private GameObject panel;
 	private GameObject operacao1;
 	private GameObject operacao2;
 	private GameObject operacao3;
@@ -22,6 +26,7 @@ public class HitCoin : MonoBehaviour
 	private GameObject player;
 	private GameObject pontos;
 	private GameObject fase;
+	private GameObject time;
 	private GameObject[] qntCoin;
 	private int tentativas = 3;
 
@@ -34,7 +39,7 @@ public class HitCoin : MonoBehaviour
 	{
 		mAudioSource = GetComponent<AudioSource>();
 		operacao = GameObject.FindWithTag("Operacao");
-		canvas = GameObject.FindWithTag("Canvas");
+		panel = GameObject.FindWithTag("Panel");
 		operacao1 = GameObject.FindWithTag("Operacao1");
 		operacao2 = GameObject.FindWithTag("Operacao2");
 		operacao3 = GameObject.FindWithTag("Operacao3");
@@ -42,39 +47,49 @@ public class HitCoin : MonoBehaviour
 		player = GameObject.FindWithTag("Player");
 		pontos = GameObject.FindWithTag("Pontos");
 		fase = GameObject.FindWithTag("Fase");
+		time = GameObject.FindWithTag("Timer");
 
-		fase.GetComponent<TextMesh>().text = (sc.getCount() + 1).ToString();
+		fase.GetComponent<TextMeshProUGUI>().text = (sc.getCount() + 1).ToString();
 	}
 
 	void Update()
     {
 		qntCoin = GameObject.FindGameObjectsWithTag("Coin");
-		if(qntCoin.Length == 0)
+		if( resposta == true)
         {
-			Debug.Log("YOU WIN");
+			win.rectTransform.localScale = new Vector3(1, 1, 1);
+			//restart(5);
 			Scene scene = SceneManager.GetActiveScene();
 			sc.IncrementSceneLoad(scene, LoadSceneMode.Additive);
 			SceneManager.LoadScene(scene.name);
 		}
-		if(tentativas == 0)
+		if(tentativas == 0 || time.GetComponent<TextMeshProUGUI>().text == "0")
         {
-			Debug.Log("YOU LOOSE");
+			lose.rectTransform.localScale = new Vector3(1, 1, 1);
 			Scene scene = SceneManager.GetActiveScene();
 			SceneManager.LoadScene(scene.name);
 		}
 	}
 
+	public void restart(float t)
+    {
+		while(t != 0)
+        {
+			t -= 1 * Time.deltaTime;
+		}
+    }
     public void ClickMenu(Button btn)
     {
 		if (btn.GetComponentInChildren<TextMeshProUGUI>().text == op.getResposta())
         {
 			btn.image.color = Color.green;
 
-			pontos.GetComponent<TextMesh>().text = (int.Parse(pontos.GetComponent<TextMesh>().text) + 1).ToString();
+			pontos.GetComponent<TextMeshProUGUI>().text = (int.Parse(pontos.GetComponent<TextMeshProUGUI>().text) + 1).ToString();
 			player.GetComponent<SimpleSampleCharacterControl>().enabled = true;
 
-			operacao.GetComponent<TextMesh>().characterSize = 0;
-			canvas.GetComponent<Canvas>().scaleFactor = 0;
+			operacao.GetComponent<TextMeshProUGUI>().fontSize = 0;
+			panel.GetComponent<RectTransform>().localScale = new Vector3(0, 1, 1);
+			resposta = true;
 		}
         else
         {
@@ -85,8 +100,8 @@ public class HitCoin : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
 	{
+		resposta = false;
 		tentativas = 3;
-
 		operacao1.GetComponent<Button>().image.color = Color.white;
 		operacao2.GetComponent<Button>().image.color = Color.white;
 		operacao3.GetComponent<Button>().image.color = Color.white;
@@ -103,10 +118,10 @@ public class HitCoin : MonoBehaviour
 
 			op.GerarOperacoes();
 
-			operacao.GetComponent<TextMesh>().text = op.getExpressao();
-			operacao.GetComponent<TextMesh>().characterSize = 0.5f;
+			operacao.GetComponent<TextMeshProUGUI>().text = op.getExpressao();
+			operacao.GetComponent<TextMeshProUGUI>().fontSize = 36;
 
-			canvas.GetComponent<Canvas>().scaleFactor = 1;
+			panel.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
 
 			operacao1.GetComponentInChildren<TextMeshProUGUI>().text = op.getOpcao();
 			operacao2.GetComponentInChildren<TextMeshProUGUI>().text = op.getOpcao();
